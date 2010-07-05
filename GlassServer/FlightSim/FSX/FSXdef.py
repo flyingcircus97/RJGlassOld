85efb031d45b1eb22b0b5c21e8c564cd4df955df
Michael LaBrie <lodilabrie@gmail.com>
2010-07-04 21:34:25 -0400

    Moved part of FSXdef to setup folder. -- To make more modular

--- GlassServer/FlightSim/FSX/FSXdef.py
+++ GlassServer/FlightSim/FSX/FSXdef.py
@@ -4,6 +4,7 @@
 # ----------------------------------------------------------
 #Constants
 from PySimConnect import event_obj, data_obj
+import setup.radios as radios
 #DataTypes
 INT32 = 1
 INT64 = 2
@@ -15,66 +16,22 @@ STRING64 = 7
 STRING128 = 8
 STRING256 = 9
 
-def freqtoBCD(freq):
-    #freq is a float.
-    #Returns BCD value as int. Needed for FSX Event to set it.
-    #freq = 123.45
-    temp = int(round(freq * 1000)) #round to 1/1000th place.
-    temp = temp / 10 #truncate to 1/100th place.
-    temp = temp % 10000 #nock off hundred place of freq
-       
-    #temp = 2345
-    bcd = 0
-    for i in range(4):
-        bcd = bcd * 16
-        bcd+= temp / 1000
-        temp = temp % 1000  * 10
-    return bcd
-
-def xpdrtoBCD(freq):
-    bcd = 0
-    temp = int(freq)
-    for i in range(4):
-        bcd = bcd * 16
-        bcd+= temp / 1000
-        temp = temp % 1000  * 10
-    print bcd
-    return bcd
-
-def adftoBCD(freq):
-    
-    bcd = 0
-    temp = int(freq * 10)
-    for i in range(5):
-        print bcd,temp, temp / 10000
-        bcd = bcd * 16
-        bcd+= temp / 10000
-        temp = temp % 10000 * 10
-    #print bcd * 16^4
-    #return 50679808
-    return bcd * 16**3
-    
+   
 
 def setup(s, variables):
+    
+        def add_var(desc, unit, type, var_name, func = None):
+            s.definition_0.add(desc, unit, type, variables.byName(var_name).data, func)
+            variables.byName(var_name).writeable = False #For FSX set writeable to False
+        
         s.definition_0 = s.create_DataDefinition(2)
         #Data definition ID 2, is the high priority data, that needs to have no delay.
-        s.definition_0.add("Airspeed Indicated", "knots", FLOAT32, variables.byName('IAS').data)
-        s.definition_0.add("GROUND VELOCITY", "knots", FLOAT32, variables.byName('GS').data)
+       
+        add_var("Airspeed Indicated", "knots", FLOAT32, 'IAS')
+        add_var("GROUND VELOCITY", "knots", FLOAT32, 'GS')
         #Radios
-        s.definition_0.add("NAV ACTIVE FREQUENCY:1", "MHZ", FLOAT32, variables.byName('NAV1_ACTIVE').data)
-        s.definition_0.add("NAV STANDBY FREQUENCY:1", "MHZ", FLOAT32, variables.byName('NAV1_STANDBY').data)
-        s.definition_0.add("NAV ACTIVE FREQUENCY:2", "MHZ", FLOAT32, variables.byName('NAV2_ACTIVE').data)
-        s.definition_0.add("NAV STANDBY FREQUENCY:2", "MHZ", FLOAT32, variables.byName('NAV2_STANDBY').data)
-        s.definition_0.add("COM ACTIVE FREQUENCY:1", "MHZ", FLOAT32, variables.byName('COM1_ACTIVE').data)
-        s.definition_0.add("COM STANDBY FREQUENCY:1", "MHZ", FLOAT32, variables.byName('COM1_STANDBY').data)
-        s.definition_0.add("COM ACTIVE FREQUENCY:2", "MHZ", FLOAT32, variables.byName('COM2_ACTIVE').data)
-        s.definition_0.add("COM STANDBY FREQUENCY:2", "MHZ", FLOAT32, variables.byName('COM2_STANDBY').data)
-        s.definition_0.add("ADF ACTIVE FREQUENCY:1", "KHZ", FLOAT32, variables.byName('ADF1_ACTIVE').data)
-        s.definition_0.add("ADF STANDBY FREQUENCY:1", "KHZ", FLOAT32, variables.byName('ADF1_STANDBY').data)
-        s.definition_0.add("ADF ACTIVE FREQUENCY:2", "KHZ", FLOAT32, variables.byName('ADF2_ACTIVE').data)
-        s.definition_0.add("ADF STANDBY FREQUENCY:2", "KHZ", FLOAT32, variables.byName('ADF2_STANDBY').data)
-        
-        s.definition_0.add("TRANSPONDER CODE:1", "", INT32, variables.byName('XPDR').data)
+        radios.setup(add_var)
+
 
 def setup_events(sevent, variables):        
     
@@ -90,17 +47,4 @@ def setup_events(sevent, variables):
         sevent.eventlist = sevent.create_EventList()
         #Setup Events, (For FSX variables that can only be written this way.)
         #Radio Events
-        add_event('NAV1_RADIO_SET', 'NAV1_ACTIVE', freqtoBCD)
-        add_event('NAV1_STBY_SET', 'NAV1_STANDBY', freqtoBCD)
-        add_event('NAV2_RADIO_SET', 'NAV2_ACTIVE', freqtoBCD)
-        add_event('NAV2_STBY_SET', 'NAV2_STANDBY', freqtoBCD)
-        add_event('COM_RADIO_SET', 'COM1_ACTIVE',  freqtoBCD)
-        add_event('COM_STBY_RADIO_SET', 'COM1_STANDBY', freqtoBCD)
-        add_event('COM2_RADIO_SET', 'COM2_ACTIVE', freqtoBCD)
-        add_event('COM2_STBY_RADIO_SET', 'COM2_STANDBY', freqtoBCD)
-        add_event('XPNDR_SET','XPDR', xpdrtoBCD)
-        add_event('ADF_COMPLETE_SET','ADF1_ACTIVE', adftoBCD)
-        add_event('ADF2_COMPLETE_SET','ADF2_ACTIVE', adftoBCD)
-        #COM2_STBY_RADIO_SET
-        #print type(Nav1_Active.set_value)
-#Nav1_Active = data_obj(0)      
+        radios.setup_events(add_event)
---
+++ GlassServer/FlightSim/FSX/setup/radios.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------
+# FSX Definitions 
+# ----------------------------------------------------------
+#Constants
+#from PySimConnect import event_obj, data_obj
+import radios
+#DataTypes
+INT32 = 1
+INT64 = 2
+FLOAT32 = 3
+FLOAT64 = 4
+STRING8 = 5
+STRING32 = 6
+STRING64 = 7
+STRING128 = 8
+STRING256 = 9
+
+def freqtoBCD(freq):
+    #freq is a float.
+    #Returns BCD value as int. Needed for FSX Event to set it.
+    #freq = 123.45
+    temp = int(round(freq * 1000)) #round to 1/1000th place.
+    temp = temp / 10 #truncate to 1/100th place.
+    temp = temp % 10000 #nock off hundred place of freq
+       
+    #temp = 2345
+    bcd = 0
+    for i in range(4):
+        bcd = bcd * 16
+        bcd+= temp / 1000
+        temp = temp % 1000  * 10
+    return bcd
+
+def xpdrtoBCD(freq):
+    bcd = 0
+    temp = int(freq)
+    for i in range(4):
+        bcd = bcd * 16
+        bcd+= temp / 1000
+        temp = temp % 1000  * 10
+    print bcd
+    return bcd
+
+def adftoBCD(freq):
+    
+    bcd = 0
+    temp = int(freq * 10)
+    for i in range(5):
+        print bcd,temp, temp / 10000
+        bcd = bcd * 16
+        bcd+= temp / 10000
+        temp = temp % 10000 * 10
+    #print bcd * 16^4
+    #return 50679808
+    return bcd * 16**3
+
+def converttoBool(value):
+    if value == 0:
+        return 0
+    else:
+        return 1
+    
+
+def setup(add_var):
+        #Radios freq
+        add_var("NAV ACTIVE FREQUENCY:1", "MHZ", FLOAT32,  'NAV1_ACTIVE')
+        add_var("NAV STANDBY FREQUENCY:1", "MHZ", FLOAT32,  'NAV1_STANDBY')
+        add_var("NAV ACTIVE FREQUENCY:2", "MHZ", FLOAT32,  'NAV2_ACTIVE')
+        add_var("NAV STANDBY FREQUENCY:2", "MHZ", FLOAT32,  'NAV2_STANDBY')
+        add_var("COM ACTIVE FREQUENCY:1", "MHZ", FLOAT32,  'COM1_ACTIVE')
+        add_var("COM STANDBY FREQUENCY:1", "MHZ", FLOAT32,  'COM1_STANDBY')
+        add_var("COM ACTIVE FREQUENCY:2", "MHZ", FLOAT32,  'COM2_ACTIVE')
+        add_var("COM STANDBY FREQUENCY:2", "MHZ", FLOAT32,  'COM2_STANDBY')
+        add_var("ADF ACTIVE FREQUENCY:1", "KHZ", FLOAT32,  'ADF1_ACTIVE')
+        add_var("ADF STANDBY FREQUENCY:1", "KHZ", FLOAT32,  'ADF1_STANDBY')
+        add_var("ADF ACTIVE FREQUENCY:2", "KHZ", FLOAT32,  'ADF2_ACTIVE')
+        add_var("ADF STANDBY FREQUENCY:2", "KHZ", FLOAT32,  'ADF2_STANDBY')
+        #Transponder
+        add_var("TRANSPONDER CODE:1", "", INT32,  'XPDR')
+        #Nav1
+        add_var("Nav OBS:1", "degrees", INT32,  'NAV1_OBS')
+        add_var("Nav Radial:1", "degrees", FLOAT32,  'NAV1_RADIAL')
+        add_var("Nav CDI:1", "number", INT32,  'NAV1_CDI')
+        add_var("Nav GSI:1", "number", INT32,  'NAV1_GSI')
+        add_var("Nav has Nav:1", "bool", INT32,  'NAV1_hasNAV', converttoBool)
+        add_var("Nav has Localizer:1", "bool", INT32,  'NAV1_hasLOC', converttoBool)
+        add_var("Nav has Glide Slope:1", "bool", INT32,  'NAV1_hasGS', converttoBool)
+        add_var("Nav DME:1", "Nautical Miles", FLOAT32,  'NAV1_DME')
+        add_var("NAV TOFROM:1", "Enum", INT32,  'NAV1_TOFROM')
+        add_var("Nav Ident:1", "", STRING8,  'NAV1_ID')
+        #Nav2
+        add_var("Nav OBS:2", "degrees", INT32,  'NAV2_OBS')
+        add_var("Nav Radial:2", "degrees", FLOAT32,  'NAV2_RADIAL')
+        add_var("Nav CDI:2", "number", INT32,  'NAV2_CDI')
+        add_var("Nav GSI:2", "number", INT32,  'NAV2_GSI')
+        add_var("Nav has Nav:2", "bool", INT32,  'NAV2_hasNAV', converttoBool)
+        add_var("Nav has Localizer:2", "bool", INT32,  'NAV2_hasLOC', converttoBool)
+        add_var("Nav has Glide Slope:2", "bool", INT32,  'NAV2_hasGS', converttoBool)
+        add_var("Nav DME:2", "Nautical Miles", FLOAT32,  'NAV2_DME')
+        add_var("NAV TOFROM:2", "Enum", INT32,  'NAV2_TOFROM')
+        add_var("Nav Ident:2", "", STRING8,  'NAV2_ID')
+
+def setup_events(add_event):        
+    
+        
+        #Radio Events
+        add_event('NAV1_RADIO_SET', 'NAV1_ACTIVE', freqtoBCD)
+        add_event('NAV1_STBY_SET', 'NAV1_STANDBY', freqtoBCD)
+        add_event('NAV2_RADIO_SET', 'NAV2_ACTIVE', freqtoBCD)
+        add_event('NAV2_STBY_SET', 'NAV2_STANDBY', freqtoBCD)
+        add_event('COM_RADIO_SET', 'COM1_ACTIVE',  freqtoBCD)
+        add_event('COM_STBY_RADIO_SET', 'COM1_STANDBY', freqtoBCD)
+        add_event('COM2_RADIO_SET', 'COM2_ACTIVE', freqtoBCD)
+        add_event('COM2_STBY_RADIO_SET', 'COM2_STANDBY', freqtoBCD)
+        add_event('XPNDR_SET','XPDR', xpdrtoBCD)
+        add_event('ADF_COMPLETE_SET','ADF1_ACTIVE', adftoBCD)
+        add_event('ADF2_COMPLETE_SET','ADF2_ACTIVE', adftoBCD)
+        add_event('VOR1_SET','NAV1_OBS')
+        add_event('VOR2_SET','NAV2_OBS')
