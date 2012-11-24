c710248378e08842f6f6cce5f9346d8c8d6c1033
Live session user <xubuntu@xubuntu.(none)>
2012-11-24 05:43:14 +0000

    Added Altimeter gauge -- Partially completed.

--- GlassClient/gauges/PFD/main.py
+++ GlassClient/gauges/PFD/main.py
@@ -11,6 +11,7 @@ import variable
 #Gauge specific imports
 import AHorizon
 import SpeedTape
+import Altimeter
 
 class gauge(gauge_parent):
     
@@ -22,6 +23,7 @@ class gauge(gauge_parent):
         #Init Parts
         self.AHorizon = AHorizon.gauge_c((500,500),(0,0), parent = self)
         self.SpeedTape = SpeedTape.gauge_c((150,500),(-325,0), parent= self)
+        self.Altimeter = Altimeter.gauge_c((150,500),(325,0), parent = self)
         #Init Variables
         
         
@@ -32,6 +34,7 @@ class gauge(gauge_parent):
             self.draw_border()
             self.AHorizon.on_draw()
             self.SpeedTape.on_draw()
+            self.Altimeter.on_draw()
             #self.SpeedTape.draw_border()
             self.end_gauge()
             
