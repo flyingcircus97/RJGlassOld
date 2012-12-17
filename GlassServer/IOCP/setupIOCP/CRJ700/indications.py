695f989b524f224d20b51356444e2b614e891b19
lullem <lutz.mehrlaender@gmx.de>
2012-12-17 21:47:30 +0100

    Added variables for VSPDS, DH and MDA fot testing. They may need to be moved, grouped later and offsets may need to be adjusted.
    Signed-off-by: lullem <lutz.mehrlaender@gmx.de>

--- GlassServer/IOCP/setupIOCP/CRJ700/indications.py
+++ GlassServer/IOCP/setupIOCP/CRJ700/indications.py
@@ -8,8 +8,15 @@ def setup(add_var):
         #Radios 
         add_var(101, 'MSTRWARN_IND', True, True)
         add_var(102, 'MSTRCAUT_IND', True, True)
-        
-        
+        add_var(103, 'VSPD_SELECTED', True, True)
+        add_var(104, 'VT', True, True)
+        add_var(105, 'V1', True, True)
+        add_var(106, 'V2', True, True)
+        add_var(107, 'VR', True, True)
+        add_var(108, 'DH_CPT', True, True)
+        add_var(109, 'MDA_CPT', True, True)
+        add_var(110, 'DH_FO', True, True)
+        add_var(111, 'MDA_FO', True, True)
 def setup_events(add_event):        
     
         
